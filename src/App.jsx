c2809699520efdcb93e4368123913bdc4a1dbc48
Alex <phithounsavanh@gmail.com>
2023-06-06 13:12:12 -0400

    setup tailwind and first init

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+
+
+const App = () => {
+  return (
+    <div className="main">
+      <h2 className="main-header">React Crud Operations</h2>
+      <h1>Hello</h1>
+    </div>
+  );
+}
+
+export default App;
